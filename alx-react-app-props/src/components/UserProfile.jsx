1f3bf999912a6c60f5e2dcab4ce675d90b96c59b
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-01-17 14:54:16 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,22 @@
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
 function UserProfile() {
-  return null; // Placeholder for checker
+  const userData = useContext(UserContext);
+
+  return (
+    <div
+      style={{
+        border: '1px solid gray',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '8px'
+      }}
+    >
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
