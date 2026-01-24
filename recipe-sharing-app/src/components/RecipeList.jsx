dc600f678b5b0e6e7200272c21e7b41546de3187
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-01-24 22:06:37 +0300

    Update RecipeList.jsx
    Update RecipeList to show filteredRecipes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,17 @@
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const recipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
       <h2>Recipes</h2>
-      {recipes.length === 0 && <p>No recipes added yet.</p>}
+      {recipes.length === 0 && <p>No recipes found.</p>}
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-         <a href={`/recipes/${recipe.id}`}>
-  <h3>{recipe.title}</h3>
-</a>
+          <a href={`/recipes/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </a>
           <p>{recipe.description}</p>
         </div>
       ))}
