a41ee74358217dfa06e0e3bcc37b80b2cd88f8e1
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-01-31 16:28:28 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Search for GitHub users using the GitHub API</p>
+    </div>
+  );
+}
+
+export default App;
