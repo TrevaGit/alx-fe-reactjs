9d6250dfd3f0bb3e22b5a0c2f8f346f5d6238728
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-02-22 02:30:49 +0300

    Create TodoList.test.jsx

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a project")).toBeInTheDocument();
+  });
+
+  test("can add a new todo", () => {
+    render(<TodoList />);
+    fireEvent.change(screen.getByPlaceholderText("Add new todo"), {
+      target: { value: "Test Todo" },
+    });
+    fireEvent.click(screen.getByText("Add"));
+    expect(screen.getByText("Test Todo")).toBeInTheDocument();
+  });
+
+  test("can toggle todo completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("can delete a todo", () => {
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText("Delete")[0];
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+});
