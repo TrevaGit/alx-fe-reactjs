3ab03207a025f86a918175f83a406c1bda9bc4ba
Treva Onyango Aoko <trevaaoko1235@gmail.com>
2026-02-22 02:14:56 +0300

    Update ProtectedRoute.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth"; // import hook
 
 function ProtectedRoute({ children }) {
-  const isAuthenticated = false; // Change to true to simulate login
+  const { isAuthenticated } = useAuth(); // use hook
   return isAuthenticated ? children : <Navigate to="/login" />;
 }
 
